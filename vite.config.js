7914187f5913c1e32f326392ca04142b972194ab
slokjii155 <chotuuuji138@gmail.com>
2026-08-08 06:52:28 +0530

    Fix Vite root configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,2 @@
 import { defineConfig } from "vite";
-
-export default defineConfig({
-    root: "web",
-    build: {
-        outDir: "dist",
-        emptyOutDir: true
-    }
-});
+export default defineConfig({ root: "web" });
