df4d4ca5622d8f6381de85c7d1b00eab06932213
slokjii155 <chotuuuji138@gmail.com>
2026-08-08 09:42:58 +0530

    Configure multi-page VCE build

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,14 @@
-import { defineConfig } from "vite";
-export default defineConfig({ root: "web" });
+﻿import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: "web",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), "web/index.html"),
+        explorer: resolve(process.cwd(), "web/explorer.html")
+      }
+    }
+  }
+});
